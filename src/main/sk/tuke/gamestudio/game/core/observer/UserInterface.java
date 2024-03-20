52d2819d7dbde3cabf2733c6a115abd4ce3890ba
Yurii Nachkebiia <yurii.nachkebiia@student.tuke.sk>
2024-03-20 08:01:19 +0100

    Delete UserInterface.java

--- src/main/sk/tuke/gamestudio/game/core/observer/UserInterface.java
+++
@@ -1,7 +0,0 @@
-package main.sk.tuke.gamestudio.game.core.observer;
-
-public interface UserInterface {
-    void displayMessage(String message);
-    String requestInput();
-    void onGameEnd(int score);
-}
